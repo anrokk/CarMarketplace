27a2af2cc16f6c5d8c7201ed036789685fc7b0d2
anrokk <anrokk@taltech.ee>
2025-01-28 10:21:18 +0100

    Base

---
+++ CarMarketplace/DAL/AppDbContext.cs
@@ -0,0 +1,38 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext : DbContext
+{
+    
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+        //SOLUTION FOLDERIST
+        // dotnet ef migrations add InitialCreate --project DAL --startup-project WebApp
+        // dotnet ef database update --project DAL --startup-project WebApp 
+        // dotnet ef database drop --project DAL --startup-project WebApp 
+        
+        // installeerida macil !!!!!!!!!!!!!!!!!
+        // dotnet tool install --global dotnet-aspnet-codegenerator
+        
+        // SCAFFOLDING - cd WebApp
+        // dotnet aspnet-codegenerator razorpage -m Contributor -outDir Pages/Contributors -dc AppDbContext -udl --referenceScriptLibraries
+        // 
+        // dotnet aspnet-coddotnet aspnet-codegenerator razorpage -m Dj -outDir Pages/Djs -dc AppDbContext -udl --referenceScriptLibrariesegenerator razorpage -m Set -outDir Pages/Sets -dc AppDbContext -udl --referenceScriptLibraries
+        
+        
+        // dotnet aspnet-codegenerator razorpage -m Set -outDir Pages/Sets -dc AppDbContext -udl --referenceScriptLibraries
+        // dotnet aspnet-codegenerator razorpage -m SetTrack -outDir Pages/SetTracks -dc AppDbContext -udl --referenceScriptLibraries
+        // dotnet aspnet-codegenerator razorpage -m Track -outDir Pages/Tracks -dc AppDbContext -udl --referenceScriptLibraries
+        // dotnet aspnet-codegenerator razorpage -m TrackContributor -outDir Pages/TrackContributors -dc AppDbContext -udl --referenceScriptLibraries
+        
+        // Käivitada need read WebAppist. cd WebApp
+        
+        // Lehel olles too layoutis lingid nähtavale endale
+        
+        // Kui teed andmebaasi muudatusi, siis pane -f scaffolding asjadele taha, et üle kirjutaks
+    }
+}
